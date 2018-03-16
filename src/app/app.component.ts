65b79b275b4efcb9a10c354ed2e288e254df7931
Mike Ekert <mike.ekert@totalexpert.com>
2018-03-16 01:29:27 -0500

    mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,16 @@ import {Component, OnInit, ViewChild, AfterViewInit} from '@angular/core';
 export class AppComponent implements AfterViewInit, OnInit {
   title = 'app';
 chills: any;
+played: any;
 ngOnInit() {
   this.chills = false;
 }
 
   ngAfterViewInit() {
+  }
+  public play() {
     const audio = new Audio('../assets/ghostadventures.mp3');
     audio.play();
-
+    this.played = true;
   }
 }
